ed0a8d46a491db6d83f055a6043509fdb7bc5b7a
Manuele Menozzi <manuele.menozzi@gmail.com>
2014-10-31 19:28:54 +0100

    Now inherits default config to websites and stores

--- src/Model/Config.php
+++ src/Model/Config.php
@@ -23,6 +23,65 @@ class Webgriffe_Config_Model_Config extends Mage_Core_Model_Config
         $file = $etcDir . DS . self::OVERRIDE_FILENAME;
         $merge = clone $this->_prototype;
         $merge->loadFile($file);
+
+        $websites = array_keys($this->getNode('websites')->asArray());
+
+        $this->_inheritDefaultConfigToWebsites($merge, $websites);
+        $this->_inheritWebsitesConfigToStores($websites, $merge);
+
         $this->extend($merge);
+    }
+
+    protected function _flatConfig(Varien_Simplexml_Element $node, array $flat = array(), $flatKey = '')
+    {
+        if ($node->hasChildren()) {
+            foreach ($node->children() as $key => $child) {
+                /** @var $child Varien_Simplexml_Element */
+                if ($child->hasChildren()) {
+                    $flat = $this->_flatConfig($child, $flat, $flatKey . $key . '/');
+                    continue;
+                }
+
+                $flat[$flatKey . $key] = (string)$child;
+            }
+        }
+
+        return $flat;
+    }
+
+    /**
+     * @param $merge
+     * @param $websites
+     */
+    protected function _inheritDefaultConfigToWebsites($merge, $websites)
+    {
+        $flattenDefault = $this->_flatConfig($merge->getNode('default'));
+        foreach ($flattenDefault as $path => $value) {
+            foreach ($websites as $website) {
+                $merge->setNode('websites/' . $website . '/' . $path, $value, false);
+            }
+        }
+    }
+
+    /**
+     * @param $websites
+     * @param $merge
+     */
+    protected function _inheritWebsitesConfigToStores($websites, $merge)
+    {
+        foreach ($websites as $websiteCode) {
+            $website = Mage::getModel('core/website')->load($websiteCode);
+            $stores = $website->getStores();
+            if ($websiteCode === 'admin') {
+                $stores = array(Mage::getModel('core/store')->load('admin'));
+            }
+            foreach ($stores as $store) {
+                $store = $store->getCode();
+                $flattenWebsite = $this->_flatConfig($merge->getNode('websites/' . $websiteCode));
+                foreach ($flattenWebsite as $path => $value) {
+                    $merge->setNode('stores/' . $store . '/' . $path, $value, false);
+                }
+            }
+        }
     }
 } 
