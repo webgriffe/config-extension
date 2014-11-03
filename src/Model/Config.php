9e6815cdd33a932cfa25d659f6cb29f8c9696f5f
Manuele Menozzi <manuele.menozzi@gmail.com>
2014-11-03 19:26:29 +0100

    Adds env specific config and base config override

--- src/Model/Config.php
+++ src/Model/Config.php
@@ -8,26 +8,59 @@
 
 class Webgriffe_Config_Model_Config extends Mage_Core_Model_Config
 {
+    const SERVER_VAR_NAME = 'MAGE_ENVIRONMENT';
     const OVERRIDE_FILENAME = 'config-override.xml';
+    const ENV_OVERRIDE_PATTERN = 'config-override-%s.xml';
+    const BASE_OVERRIDE_FILENAME = 'local-override.xml';
+    const BASE_ENV_OVERRIDE_PATTERN = 'local-override-%s.xml';
+
+    public function loadBase()
+    {
+        parent::loadBase();
+        $this->_loadOverride(self::BASE_OVERRIDE_FILENAME, false);
+        $this->_loadOverride($this->_getBaseEnvOverrideFilename(), false);
+        return $this;
+    }
+
+    /**
+     * This override is needed because in the loadModules method Magento prevents local.xml overwriting that is
+     * exactly what we have to do. So we need to re-override base configuration after the loadMoudles.
+     *
+     * @return $this|Mage_Core_Model_Config
+     */
+    public function loadModules()
+    {
+        parent::loadModules();
+        $this->_loadOverride(self::BASE_OVERRIDE_FILENAME, false);
+        $this->_loadOverride($this->_getBaseEnvOverrideFilename(), false);
+        return $this;
+    }
+
 
     public function loadDb()
     {
         parent::loadDb();
-        $this->_loadOverride();
+        $this->_loadOverride(self::OVERRIDE_FILENAME);
+        $this->_loadOverride($this->_getEnvOverrideFilename());
         return $this;
     }
 
-    protected function _loadOverride()
+    /**
+     * @param $overrideFilename
+     * @param bool $inheritConfig
+     */
+    protected function _loadOverride($overrideFilename, $inheritConfig = true)
     {
         $etcDir = $this->getOptions()->getEtcDir();
-        $file = $etcDir . DS . self::OVERRIDE_FILENAME;
+        $file = $etcDir . DS . $overrideFilename;
         $merge = clone $this->_prototype;
         $merge->loadFile($file);
 
-        $websites = array_keys($this->getNode('websites')->asArray());
-
-        $this->_inheritDefaultConfigToWebsites($merge, $websites);
-        $this->_inheritWebsitesConfigToStores($websites, $merge);
+        if ($inheritConfig) {
+            $websites = array_keys($this->getNode('websites')->asArray());
+            $this->_inheritDefaultConfigToWebsites($merge, $websites);
+            $this->_inheritWebsitesConfigToStores($websites, $merge);
+        }
 
         $this->extend($merge);
     }
@@ -35,7 +68,7 @@ class Webgriffe_Config_Model_Config extends Mage_Core_Model_Config
     protected function _flatConfig($node, array $flat = array(), $flatKey = '')
     {
         if ($node instanceof Varien_Simplexml_Element && $node->hasChildren()) {
-            foreach ($node->children() as $key => $child) {
+            foreach($node->children() as $key => $child) {
                 /** @var $child Varien_Simplexml_Element */
                 if ($child->hasChildren()) {
                     $flat = $this->_flatConfig($child, $flat, $flatKey . $key . '/');
@@ -84,4 +117,27 @@ class Webgriffe_Config_Model_Config extends Mage_Core_Model_Config
             }
         }
     }
-} 
+
+    protected function _getEnvOverrideFilename()
+    {
+        return sprintf(self::ENV_OVERRIDE_PATTERN, $this->_getCurrentEnvironment());
+    }
+
+    /**
+     * @return string Returns current environment by reading the environment variable "MAGE_ENVIRONMENT". Default
+     * environment is "prod".
+     */
+    protected function _getCurrentEnvironment()
+    {
+        if (isset($_SERVER[self::SERVER_VAR_NAME])) {
+            return $_SERVER[self::SERVER_VAR_NAME];
+        }
+
+        return 'prod';
+    }
+
+    protected function _getBaseEnvOverrideFilename()
+    {
+        return sprintf(self::BASE_ENV_OVERRIDE_PATTERN, $this->_getCurrentEnvironment());
+    }
+}
